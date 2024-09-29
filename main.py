16506e881e115baf49896a1f55efdfbb3508456b
Adrian030304 <adrianromanenco1@gmail.com>
2024-09-29 21:54:47 +0300

    document more on what the library does and what is being returned

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ api_link = "https://catfact.ninja/fact"
 # this is an important endpoint for our get request , is is the above link of the api
 # output --> a json response from the api
 # request made -> you get a response back that contains all the info from the server
+
+# requests.get() send a get request to the server , it has many arguments which can be filled up but the most important one to take not of is 
+# the api endpoint link
+
+http_response = requests.get(api_link) #returns a response object
+print(http_response)
