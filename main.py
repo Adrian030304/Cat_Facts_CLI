a21b8e4e5fcfa58a6406f2b6002addffa52f39c1
Adrian030304 <adrianromanenco1@gmail.com>
2024-09-29 21:51:56 +0300

    small comments on how the requests work

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import requests
 
 #now I will create the link I will be using for the api url
 api_link = "https://catfact.ninja/fact"
+
+# this is an important endpoint for our get request , is is the above link of the api
+# output --> a json response from the api
+# request made -> you get a response back that contains all the info from the server
